914f5298cc464a9257fa996e71f7b774a4d6cce5
Jerry790790 <jerrychinyongxuan790@gmail.com>
2026-08-24 23:16:24 +0800

    fixed the order item id error

--- analysis/report_categories.sql
+++ analysis/report_categories.sql
@@ -10,7 +10,7 @@ CREATE PROCEDURE sp_report_top_categories(IN p_limit INT)
 BEGIN
     SELECT 
         p.product_category_name AS Category,
-        COUNT(oi.item_id) AS Total_Units_Sold,
+        COUNT(oi.order_item_id) AS Total_Units_Sold,
         SUM(fn_calculate_order_total(oi.price, oi.freight_value)) AS Total_Revenue
     FROM order_items oi
     JOIN products p ON oi.product_id = p.product_id
